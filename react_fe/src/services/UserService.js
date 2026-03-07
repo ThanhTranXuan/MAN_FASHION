b0830591ab0fde5e734d703472d09dce47784d2a
tranxuanthanhjob <tranquockhanhxxx@gmail.com>
2026-03-07 19:54:09 +0700

    clear production branch

--- react_fe/src/services/UserService.js
+++
@@ -1,8 +0,0 @@
-import ApiClient from 'api/ApiClient';
-import ApiUrl from 'constants/ApiUrl';
-
-const UserService = {
-  getAll: (params) => ApiClient.get(ApiUrl.ALL_USERS, { params }),
-};
-
-export default UserService;
--- react_fe/src/views/user/home/components/MotionPrimitives.js
+++
@@ -1,7 +0,0 @@
-import { Flex, Text, Image }  from '@chakra-ui/react'; 
-
-import { motion } from 'framer-motion';
-
-export const MotionFlex = motion(Flex);
-export const MotionText = motion(Text);
-export const MotionImage = motion(Image);
